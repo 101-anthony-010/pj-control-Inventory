c0868104c34017b7e623d6b2ff8797f73118e865
antony <gonzalescamposantony@gmail.com>
2024-03-12 07:20:26 -0500

    table view

--- src/components/ProductTableComponent.jsx
+++ src/components/ProductTableComponent.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const ProductTableComponent = () => {
   return (
-    <div>ProductTableComponent</div>
+    <table className="w-full text-center">
+      <thead>
+        <tr className="bg-gray-800 text-white">
+          <th className="px-4 py-2">Nombre</th>
+          <th className="px-4 py-2">Column 2</th>
+          <th className="px-4 py-2">Column 3</th>
+          <th className="px-4 py-2">Column 3</th>
+          <th className="px-4 py-2">Column 3</th>
+          <th className="px-4 py-2">Column 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border px-4 py-2">Row 1, Column 1</td>
+          <td className="border px-4 py-2">Row 1, Column 2</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+        </tr>
+        <tr>
+          <td className="border px-4 py-2">Row 2, Column 1</td>
+          <td className="border px-4 py-2">Row 2, Column 2</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 2, Column 3</td>
+        </tr>
+        <tr>
+          <td className="border px-4 py-2">Row 3, Column 1</td>
+          <td className="border px-4 py-2">Row 3, Column 2</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 1, Column 3</td>
+          <td className="border px-4 py-2">Row 3, Column 3</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
