fc9347a118e63015dcaad0ad738faae8ec2d56f0
101-anthony-010 <gonzalescamposantony@gmail.com>
2024-03-21 22:07:10 -0500

    noche

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <navbar className='flex justify-center items-center gap-10 p-4'>
+    <nav className='flex justify-center items-center gap-10 p-4'>
       <p>logo</p>
       <Link to={'/asignation'}>Asignacion</Link>
       <Link to={'/product'}>Producto</Link>
       <Link to={'/user'}>Usuario</Link>
-    </navbar>
+    </nav>
   )
 }
 
--- src/pages/PageAsignation.jsx
+++ src/pages/PageAsignation.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const PageAsignation = () => {
   return (
-    <div>PageAsignation</div>
+    <>
+    <table className="w-full text-center">
+      <thead>
+        <tr className="bg-gray-800 text-white">
+          <th className="px-4 py-2">ID</th>
+          <th className="px-4 py-2">Marca</th>
+          <th className="px-4 py-2">Modelo ID</th>
+          <th className="px-4 py-2">Número de Serie</th>
+          <th className="px-4 py-2">Usuario ID</th>
+          <th className="px-4 py-2">Fecha</th>
+          <th className="px-4 py-2">Descripción</th>
+          <th className="px-4 py-2">Cantidad</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+    </>
   )
 }
 
